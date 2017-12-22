ae0f8abe123a4f5c885c7125ad24da9cb018aa90
zaaack <zaaack@qq.com>
2017-12-22 14:41:29 +0800

    Fix persist type

--- src/enhancers/persist.ts
+++ src/enhancers/persist.ts
@@ -8,6 +8,12 @@ export default function withPersist<State, Actions>({
   deserialize = JSON.parse,
   debounce = 50,
   key = 'hydux-persist',
+}: {
+  store?: Storage,
+  serialize?: (data: any) => string,
+  deserialize?: (str: string) => any,
+  debounce?: number,
+  key?: string,
 } = {}): (app: App<State, Actions>) => App<State, Actions> {
   let timer
   return (app: App<State, Actions>) => (props: AppProps<State, Actions>) => {
